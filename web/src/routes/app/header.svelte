eff4147083f964656f2fcfa0072b9cc70d1a78dc
Thomas Gorham <tgiv014@gmail.com>
2023-04-06 19:15:34 -0700

    MORE logo

--- web/src/routes/app/header.svelte
+++ web/src/routes/app/header.svelte
@@ -1,3 +1,10 @@
-<div class="flex w-full py-2 px-4 border-b border-stone-900">
-	<p class="font-mono italic text-2xl">sugarcube</p>
+<script>
+	import Sugarcube from '../../components/Sugarcube.svelte';
+</script>
+
+<div class="flex w-full items-center gap-4 border-b border-stone-900 px-4 py-2">
+	<div class="w-8">
+		<Sugarcube style="stroke-width:20px;" />
+	</div>
+	<p class="font-mono text-2xl italic">sugarcube</p>
 </div>
